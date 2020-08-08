7c73756fd817f1f15f6e9dd39e24df6236de72b2
kimkim2951 <ty53.k.k@gmail.com>
2020-08-08 14:01:54 +0900

    [WIP]購入確認画面マークアップ

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   resources :items
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  resources :buyers, only: :index do
+    collection do
+      get 'index', to: 'buyers#index'
+      post 'pay', to: 'buyers#pay'
+      get 'done', to: 'buyers#done'
+    end
+  end
   root 'items#index'
 end
